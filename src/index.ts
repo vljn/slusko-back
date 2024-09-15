1a2b3bc075deb43898e4863ef0244a574d0f40cf
vljn <121915880+vljn@users.noreply.github.com>
2024-09-15 21:26:35 +0200

    Add a function for registering controllers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import 'reflect-metadata';
 import cookieParser from 'cookie-parser';
 
+import UsersController from './controllers/users';
+import registerControllers from './lib/registerControllers';
+import AuthController from './controllers/auth';
 
 dotenv.config();
 
@@ -10,6 +13,8 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
+registerControllers(app, [UsersController, AuthController]);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ src/lib/registerControllers.ts
@@ -0,0 +1,11 @@
+import Controller from '../controllers/controller';
+
+export default function registerControllers(
+  router: any,
+  controllers: Array<new (router: any) => Controller>
+): void {
+  controllers.forEach((controllerClass) => {
+    const controller = new controllerClass(router);
+    controller.connect();
+  });
+}

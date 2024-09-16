a07d8f0d7e5c74f1f33fd6a3893b85d403a1a9bc
vljn <121915880+vljn@users.noreply.github.com>
2024-09-17 00:46:19 +0200

    Add SongsController

---
+++ src/controllers/songs.ts
@@ -0,0 +1,50 @@
+import { Request, Response, Router } from 'express';
+import path from 'path';
+import fs from 'fs';
+
+import Controller from '../lib/baseController';
+import { Middleware, Post } from '../lib/decorators';
+import { isAdmin, isAuthenticated } from '../lib/middleware/auth';
+import uploadSong from '../config/multer';
+import ffmpeg from '../config/ffmpeg';
+import prisma from '../config/prisma';
+
+export default class SongsController extends Controller {
+  constructor(router: Router) {
+    super('/songs', router);
+  }
+
+  // TODO validation
+  // TODO errors
+
+  @Middleware([isAuthenticated, isAdmin, uploadSong.single('song')])
+  @Post('/')
+  public async uploadSong(req: Request, res: Response) {
+    const spotifyId = req.body.spotify_id;
+    const song = await prisma.song.create({ data: { spotifyId } });
+
+    console.log(req.file);
+    const file = req.file?.path as string;
+    const filename = path.parse(file).name;
+    const ext = path.parse(file).ext;
+
+    const durations = [2, 4, 9, 15, 22, 30];
+    durations.forEach((duration, index) => {
+      const clipPath = path.join('songs', filename + `clip${index}` + ext);
+      ffmpeg(req.file?.path)
+        .setStartTime('00:00:00')
+        .setDuration(duration)
+        .output(clipPath)
+        .on('end', async () => {
+          if (index === durations.length - 1) {
+            fs.rmSync(file);
+          }
+          await prisma.songClip.create({
+            data: { fileName: clipPath, order: index, song: { connect: { id: song.id } } },
+          });
+        })
+        .run();
+    });
+    res.json({ status: 'success' });
+  }
+}

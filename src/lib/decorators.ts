ea7ad6f22aab747283e283eca486f20e12a9a775
vljn <121915880+vljn@users.noreply.github.com>
2024-09-16 08:30:10 +0200

    Enforce method signature for Route and MIddleware decorator

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -1,22 +1,32 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { HTTPMethod, MiddlewareDefinition, RouteDefinition } from './types';
 
-export function Route(path: string, method: HTTPMethod): MethodDecorator {
-  return function (target: Object, propertyKey: string | Symbol) {
+type ExpressRequestHandler = (req: Request, res: Response, next?: NextFunction) => any;
+
+export function Route(path: string, method: HTTPMethod) {
+  return function (
+    target: Object,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<ExpressRequestHandler>
+  ) {
     const routes: RouteDefinition[] = Reflect.getMetadata('routes', target.constructor) || [];
-    routes.push({ method, path, methodName: propertyKey.toString() });
+    routes.push({ method, path, methodName: propertyKey });
 
     Reflect.defineMetadata('routes', routes, target.constructor);
   };
 }
 
-export function Middleware(middlewares: Array<RequestHandler>): MethodDecorator {
-  return function (target: Object, propertyKey: string | Symbol) {
+export function Middleware(middlewares: Array<RequestHandler>) {
+  return function (
+    target: Object,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<ExpressRequestHandler>
+  ) {
     const storedMiddlewares: MiddlewareDefinition[] =
       Reflect.getMetadata('middlewares', target.constructor) || [];
 
     middlewares.forEach((middleware) => {
-      storedMiddlewares.push({ methodName: propertyKey.toString(), middleware });
+      storedMiddlewares.push({ methodName: propertyKey, middleware });
     });
 
     Reflect.defineMetadata('middlewares', storedMiddlewares, target.constructor);

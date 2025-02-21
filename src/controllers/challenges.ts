f374478a8ce2b1d8745243bb9ea52b69d2813839
vljn <121915880+vljn@users.noreply.github.com>
2025-02-21 21:17:30 +0100

    Implement getAllChallenges method and refactor createChallenge to use startDate and endDate

--- src/controllers/challenges.ts
+++ src/controllers/challenges.ts
@@ -10,16 +10,35 @@ export default class ChallengesControllers extends Controller {
     super('/challenges', router);
   }
 
+  // TODO add filtering and searching
   @Get('/')
-  public async getAllChallenges(req: Request, res: Response) {}
+  public async getAllChallenges(req: Request, res: Response) {
+    const challenges = await prisma.challenge.findMany({
+      include: { category: true, song: true },
+    });
+
+    res.json({ status: 'success', challenges });
+  }
 
   @Middleware([isAuthenticated, isAdmin])
   @Post('/')
   public async createChallenge(req: Request, res: Response) {
-    const { date, max_guesses: maxGuesses, category_id: categoryId, song_id: songId } = req.body;
+    const {
+      start_date: startDate,
+      end_date: endDate,
+      max_guesses: maxGuesses,
+      category_id: categoryId,
+      song_id: songId,
+    } = req.body;
+    const dates = { start: startDate, end: endDate };
+    dates.start = new Date(startDate);
+    dates.end = dates.end
+      ? new Date(dates.end)
+      : new Date(startDate.getTime() + 1000 * 60 * 60 * 24);
     const challenge = await prisma.challenge.create({
       data: {
-        date: new Date(date),
+        startDate: dates.start,
+        endDate: dates.end,
         maxGuesses,
         category: { connect: { id: parseInt(categoryId) } },
         song: { connect: { id: parseInt(songId) } },
